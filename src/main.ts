cd16133f8629a7259c0b084a5f600b3e6e6eeedd
Deer-Canidae <91327146+Deer-Canidae@users.noreply.github.com>
2022-08-18 17:05:11 -0400

    Improved portrait mode

--- src/main.ts
+++ src/main.ts
@@ -17,15 +17,17 @@ const updateClock = () => {
   const secFormated = sec >= 10 ? `${sec}` : `0${sec}`;
   const milFormated =
     mil >= 100 ? `${mil}` : mil >= 10 ? `0${mil}` : `00${mil}`;
+  //formats to 10^(-2) s precision
+  const centiFormated =
+    mil < 100 ? `0${Math.floor(mil / 10)}` : `${Math.floor(mil / 10)}`;
 
-  const stylizedClock: string = [
-    ...`${hourFormated}:${minsFormated}:${secFormated}.${milFormated}`,
-  ]
-    .map((el) => {
-      if (el === ":" || el === ".") return `<div class="seperator">${el}</div>`;
-      return `<div>${el}</div>`;
-    })
-    .join("");
+  const stylizedClock: string = `<div>${hourFormated}</div>\
+  <div class="seperator">:</div>\
+  <div>${minsFormated}</div>\
+  <div class="seperator">:</div>\
+  <div>${secFormated}</div>\
+  <div class="seperator">.</div>\
+  <div class="milisec" data-mili="${milFormated}" data-centi="${centiFormated}"></div>`;
 
   app.innerHTML = stylizedClock;
 };
